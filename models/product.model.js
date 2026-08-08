cbcede942ce4911e44d87577dcb3377aaa3ea5b9
Haidy <haidygamal496@gmail.com>
2026-08-09 01:42:53 +0300

    Add product model and schema

---
+++ models/product.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 2
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const productModel = mongoose.model('Product', productSchema)
+
+module.exports = productModel
