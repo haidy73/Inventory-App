8dcfd3020c5eeb493978c0ec5eb14dc1b196e427
Haidy <haidygamal496@gmail.com>
2026-08-17 15:55:06 +0300

    Add logging a user in to the auth contorller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
-const bcrypt = require('bcrypt');
 const User = require('../models/user.model.js');
-const { validateRegisterInput } = require('../validations/auth.validation.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const { validateRegisterInput, validateLoginInput } = require('../validations/auth.validation.js');
 
 const register = async (req, res) => {
   try {
@@ -33,4 +35,38 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ success: false, message: 'Invalid Email or password' })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      return res.status(400).json({ success: false, message: 'Invalid Email or password' })
+    }
+
+    
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET || 'JWTSECRETKEY',
+      { expiresIn: '7h' }
+    )
+
+    res.status(200).json({
+      success: true,
+      token,
+      data: { id: user._id, name: user.username, email: user.email, role: user.role },
+    })
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message })
+  }
+};
+
+module.exports = { 
+  register, 
+  login 
+};
