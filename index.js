8554208abb6060e50413737991db8d57224ddbc1
Haidy <haidygamal496@gmail.com>
2026-08-17 13:22:10 +0300

    Add /auth route to the app

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const config = require('./config/env')
 const connectDB = require('./db/connect.db')
+const authRouter = require('./routes/auth.routes')
 
 const app = express()
 connectDB()
 
+app.use(express.json())
 
+app.use('/auth', authRouter)
 
 app.listen(config.port, () => {
     console.log(`Listening on port ${config.port}`)
