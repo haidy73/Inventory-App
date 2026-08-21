70837765021089f3b6130cf4685cdac6a42c29a5
Haidy <haidygamal496@gmail.com>
2026-08-22 00:31:41 +0300

    feat: add product list to the app.routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
+import { ProductList } from './features/products/product-list/product-list';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path:'register',
     component:Register,
     title:'register'
+  },
+  {
+    path:'products',
+    component:ProductList,
+    title: 'products'
   },
 ];
